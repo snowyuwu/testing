4b48b31db1304ce9535ad768403c99494c50ebd6
snowy <jiayeeyo@gmail.com>
2022-07-07 23:42:50 +0800

    20220707 third commit

--- js/main.js
+++ js/main.js
@@ -14,16 +14,6 @@
     
     // Initiate the wowjs
     new WOW().init();
-
-
-    // Sticky Navbar
-    // $(window).scroll(function () {
-    //     if ($(this).scrollTop() > 45) {
-    //         $('.navbar').addClass('sticky-top shadow-sm');
-    //     } else {
-    //         $('.navbar').removeClass('sticky-top shadow-sm');
-    //     }
-    // });
     
     // Dropdown on mouse hover
     const $dropdown = $(".dropdown");
